03c7a231bebc474345bbe92d4357f0cf0d12fbe8
NatividadeGuilherme <guilhermenatividade450@hotmail.com>
2019-02-08 18:05:33 -0200

    Começando a incluir as ocorrências dos alunos

---
+++ GrupoEstudosMusical.Models/Entities/Ocorrencia.cs
@@ -0,0 +1,15 @@
+﻿
+
+using System;
+
+namespace GrupoEstudosMusical.Models.Entities
+{
+    public class Ocorrencia:BaseEntity
+    {
+        public string Titulo { get; set; }
+        public DateTime DataOcorrido { get; set; }
+
+
+
+    }
+}
