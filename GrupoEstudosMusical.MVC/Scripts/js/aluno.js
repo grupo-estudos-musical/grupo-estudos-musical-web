c125822f39d3e152880c1a82aa1a9c1f9b2adab2
NicolasSR <nicolassilva114@gmail.com>
2019-04-10 21:48:13 -0300

    Adicionando preview da imagem do aluno

---
+++ GrupoEstudosMusical.MVC/Scripts/js/aluno.js
@@ -0,0 +1,30 @@
+﻿function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        console.log(reader);
+        reader.onload = function (e) {
+            $('#imagemPreview').attr('src', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+    else {
+        var img = input.value;
+        $(input).next().attr('src', img);
+    }
+}
+
+function verificaMostraBotao() {
+    $('input[type=file]').each(function (index) {
+
+        if ($('input[type=file]').eq(index).val() !== "") {
+            readURL(this);
+
+            $('.hide').show();
+        }
+    });
+}
+
+$('input[type=file]').on("change", function () {
+    console.log(this);
+    verificaMostraBotao();
+});
