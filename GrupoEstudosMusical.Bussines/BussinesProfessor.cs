f9c006db88f81402aa6527193156d22651b7285d
nicolasrezende <nicolassilva114@gmail.com>
2019-05-20 16:04:35 -0300

    Criando usuário para professor

--- GrupoEstudosMusical.Bussines/BussinesProfessor.cs
+++ GrupoEstudosMusical.Bussines/BussinesProfessor.cs
@@ -1,7 +1,9 @@
-﻿using GrupoEstudosMusical.Models.Entities;
+﻿using GrupoEstudosMusical.Bussines.Helpers;
+using GrupoEstudosMusical.Models.Entities;
 using GrupoEstudosMusical.Models.Interfaces.Bussines;
 using GrupoEstudosMusical.Models.Interfaces.Repository;
 using System;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 
 namespace GrupoEstudosMusical.Bussines
@@ -9,10 +11,12 @@ namespace GrupoEstudosMusical.Bussines
     public class BussinesProfessor : BussinesGeneric<Professor>, IBussinesProfessor
     {
         private readonly IRepositoryProfessor _repositoryProfessor;
+        private readonly IRepositoryUsuario _repositoryUsuario;
 
-        public BussinesProfessor(IRepositoryProfessor repositoryProfessor) : base(repositoryProfessor)
+        public BussinesProfessor(IRepositoryProfessor repositoryProfessor, IRepositoryUsuario repositoryUsuario) : base(repositoryProfessor)
         {
             _repositoryProfessor = repositoryProfessor;
+            _repositoryUsuario = repositoryUsuario;
         }
 
         public override Task AlterarAsync(Professor entity)
@@ -25,6 +29,26 @@ namespace GrupoEstudosMusical.Bussines
         {
             ValidarContato(entity);
             return base.InserirAsync(entity);
+        }
+
+        public async Task<string> InserirAsync(Professor professor, Usuario usuario)
+        {
+            var hash = new HashHelper(SHA512.Create());
+            string senha;
+
+            while (true)
+            {
+                senha = SenhaHelper.GerarSenhaNumericaAleatoria();
+                var senhaExistente = await _repositoryUsuario.ObterPorSenhaAsync(hash.CriptografarSenha(senha));
+                if (senhaExistente == null)
+                    break;
+            }
+
+            usuario.Senha = hash.CriptografarSenha(senha);
+            await _repositoryUsuario.InserirAsync(usuario);
+            professor.UsuarioId = usuario.Id;
+            await InserirAsync(professor);
+            return senha;
         }
 
         private void ValidarContato(Professor professor)
