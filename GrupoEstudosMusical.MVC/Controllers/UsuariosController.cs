7e6747f0f4a42260e576b5d93a0984a6b3b50178
nicolasrezende <nicolassilva114@gmail.com>
2019-05-21 16:50:16 -0300

    Criando tela de login

---
+++ GrupoEstudosMusical.MVC/Controllers/UsuariosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GrupoEstudosMusical.MVC.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        [Route("Login")]
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
